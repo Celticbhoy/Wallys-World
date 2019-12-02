7f886034143020f735ea1086349b0c05139bfb87
Celticbhoy <dqt957@gmail.com>
2019-12-02 18:03:02 -0500

    123

--- WallysWorld/MainWindow.xaml.cs
+++ WallysWorld/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace WallysWorld
     /// </summary>
     public partial class MainWindow : Window
     {
-        DBFunction dbf = new DBFunction();
+        
         string connectionStinrg = "server=127.0.0.1; port=3306; username=root; database=dtwallysworld;";
         public MainWindow()
         {
@@ -52,6 +52,58 @@ namespace WallysWorld
 
             cnn.Close();
 
+        }
+
+        private void Button_Click_OpenAdd(object sender, RoutedEventArgs e)
+        {
+            if (addCus.Visibility == Visibility.Visible)
+            {
+                addCus.Visibility = Visibility.Collapsed;
+                SearchOrder.Visibility = Visibility.Visible;
+                UpdateLayout();
+            }
+            else
+            {
+                addCus.Visibility = Visibility.Visible;
+                SearchOrder.Visibility = Visibility.Collapsed;
+                UpdateLayout();
+            }
+        }
+
+        private void Button_Click_OrderSearch(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection cnn;
+            cnn = new MySqlConnection(connectionStinrg);
+            cnn.Open();
+            string sql;
+
+            sql = "SELECT * from Orders WHERE OrderID=@OrderID";
+            var command = new MySqlCommand(sql, cnn);
+            command.Parameters.AddWithValue("@OrderID", orderIDSearch.Text);
+            var ds = new DataSet();
+            MySqlDataAdapter mya = new MySqlDataAdapter(command);
+            mya.Fill(ds, "orderTable");
+            dataGridOrder.DataContext = ds;
+            cnn.Close();
+            
+
+        }
+
+        private void Button_Click_OpenSearch(object sender, RoutedEventArgs e)
+        {
+            if(SearchOrder.Visibility == Visibility.Visible)
+            {
+                SearchOrder.Visibility = Visibility.Collapsed;
+                addCus.Visibility = Visibility.Visible;
+                UpdateLayout();
+
+            }
+            else
+            {
+                SearchOrder.Visibility = Visibility.Visible;
+                addCus.Visibility = Visibility.Collapsed;
+                UpdateLayout();
+            }
         }
     }
 }
