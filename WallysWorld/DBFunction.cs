6affb72114a6c2d5fc94d0a48066aaf96f08ce5e
Celticbhoy <dqt957@gmail.com>
2019-12-02 14:28:55 -0500

    Add search customer

---
+++ WallysWorld/DBFunction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WallysWorld
+{
+    class DBFunction
+    {
+        string connectionStinrg = "server=127.0.0.1; port=3306 username=root;password=Conestoga1;";
+
+        public object SearchCustomer(string cusIn)
+        {
+            MySqlConnection cnn;
+            cnn = new MySqlConnection(connectionStinrg);
+            cnn.Open();
+            MySqlCommand command = cnn.CreateCommand();
+            
+            
+            command.CommandText = "SELECT * FROM Person Where Telephone like '%@tele' OR LastName = @last;";
+            MySqlDataAdapter adp = new MySqlDataAdapter(command);
+            DataSet ds = new DataSet();
+            command.Parameters.AddWithValue("@tele", cusIn);
+            command.Parameters.AddWithValue("@last", cusIn);
+            
+            command.ExecuteNonQuery();
+
+
+            adp.Fill(ds, "cusTable");
+            command.Dispose();
+            cnn.Close();
+            return ds;
+
+
+        }
+    }
+}
--- WallysWorld/MainWindow.xaml.cs
+++ WallysWorld/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace WallysWorld
 {
@@ -20,9 +22,19 @@ namespace WallysWorld
     /// </summary>
     public partial class MainWindow : Window
     {
+        DBFunction dbf = new DBFunction();
+        string connectionStinrg = "server=127.0.0.1; port=3306 username=root;password=Conestoga1;";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dataGridCustomer.DataContext = dbf.SearchCustomer(custInfo.Text);
+
         }
     }
 }
